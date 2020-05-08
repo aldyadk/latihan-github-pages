d4e1c0f7ec8a303a659617cf84220e80e1ce9451
aldyadk <aldy.andika@gmail.com>
2020-05-08 19:40:11 +0700

    done

---
+++ dogs.js
@@ -0,0 +1,22 @@
+var dogs = [{
+    name: 'Henry',
+    age: 0.5,
+    breed: 'Aussie',
+    food: 'kibble',
+    toys: ['tennis ball', 'chew toy'],
+    picture: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/03173132/Australian-Shepherd.1.jpg'
+}, {
+    name: 'Tilly',
+    age: 5,
+    breed: 'Mutt',
+    food: 'kibble',
+    toys: ['bone', 'kong', 'squeaky toy'],
+    picture: 'https://cdn3-www.dogtime.com/assets/uploads/2011/01/file_23228_mutt-460x290.jpg'
+}, {
+    name: 'Apollo',
+    age: 10,
+    breed: 'Labrador',
+    food: 'absolutely anything',
+    toys: ['old sock', 'other old sock', 'more old socks'],
+    picture: 'https://d1941uuft27pfg.cloudfront.net/breed-uploads/2018/08/Labrador-637x260.png?bust=1556226791'
+}]
